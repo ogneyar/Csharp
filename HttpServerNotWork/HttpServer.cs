cfc1dbf784f78cee57968e312ad11ffa727be9f8
ogneyar <ogneyar-ne@ya.ru>
2021-09-12 22:28:28 +0300

    Этот сервер не хочет работать(

--- HttpServerNotWork/HttpServer.cs
+++ HttpServerNotWork/HttpServer.cs
@@ -43,12 +43,13 @@ namespace HTTPServer {
     class Client {
         // Конструктор класса. Ему нужно передавать принятого клиента от TcpListener
         public Client(TcpClient Client) {
+            byte[] Buffer = new byte[256];
             // Код простой HTML-странички
             string Html = "<html><body><h1>It works!</h1></body></html>";
             // Необходимые заголовки: ответ сервера, тип и длина содержимого. После двух пустых строк - само содержимое
             string Str = "HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8\nContent-Length:" + Html.Length.ToString() + "\n\n" + Html;
             // Приведем строку к виду массива байт
-            byte[] Buffer = Encoding.UTF8.GetBytes(Str);
+            Buffer = Encoding.UTF8.GetBytes(Str);
             Console.WriteLine("vot!");
             // Отправим его клиенту
             Client.GetStream().Write(Buffer, 0, Buffer.Length);
